e0a208989b7839c8c7924155ecb465292432520b
Nice <nicenicole.in@gmail.com>
2019-07-19 12:56:01 +0200

    linked javascript sheet to my index.html file

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// var day=document.form.display.dd.value;
+// var month=mm.value;
+// var year=yy.value;
+function printDay(){
+    var day = dd.value;
+    var month=mm.value;
+    var year=yy.value;
+    
+}
